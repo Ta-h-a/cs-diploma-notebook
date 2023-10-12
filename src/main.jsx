aea36c3be6a7c9229907df1b08308d28c91a1456
Ta-h-a <tahasindoli@gmail.com>
2023-10-12 13:17:14 +0530

    Feature: Add Hero and link staff and docs page

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import './index.css';
 import theme from './components/themes/index.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Staff from './components/Staff';
+import Hero from './components/Hero.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider theme={theme}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route path="/" element={<Hero />}>
+        </Route>
+        <Route path="/docs" element={<App />}>
         </Route>
         <Route path="/staff" element={<Staff />}>
         </Route>
